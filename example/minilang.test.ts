620552c1d1c5f2838f53cfa82d8dab89e5156626
boukeversteegh <info@boukeversteegh.nl>
2023-08-25 12:19:45 +0200

    example

--- example/minilang.test.ts
+++ example/minilang.test.ts
@@ -1,16 +1,95 @@
-import {tokenize} from "./minilang";
+import {compile, parse, tokenize} from "./minilang";
 
 describe('tokenizer', () => {
     it('a = add(b, 1)', () => {
         expect(tokenize('a = add(b, 1)')).toEqual([
-            'a',
-            '=',
-            'add',
-            '(',
-            'b',
-            ',',
-            '1',
-            ')',
+            {type: 'NAME', value: 'a'},
+            {type: 'ASSIGN', value: '='},
+            {type: 'NAME', value: 'add'},
+            {type: 'OPEN', value: '('},
+            {type: 'NAME', value: 'b'},
+            {type: 'COMMA', value: ','},
+            {type: 'NUMBER', value: '1'},
+            {type: 'CLOSE', value: ')'},
         ])
     });
+});
+
+describe('parser', () => {
+    it('a = 1', () => {
+        expect(parse(tokenize('a = 1'))).toEqual({
+            type: 'ASSIGN',
+            variable: 'a',
+            value: {
+                type: 'NUMBER',
+                value: 1
+            }
+        })
+    });
+    it('a = add(b, 1)', () => {
+        expect(parse(tokenize('a = add(b, 1)'))).toEqual({
+            type: 'ASSIGN',
+            variable: 'a',
+            value: {
+                type: 'CALL',
+                name: 'add',
+                arguments: [
+                    {type: 'VARIABLE', name: 'b'},
+                    {type: 'NUMBER', value: 1}
+                ]
+            }
+        })
+    });
+    it('a = foobar(1, 2, 3)', () => {
+        expect(parse(tokenize('a = foobar(1, 2, 3)'))).toEqual({
+            type: 'ASSIGN',
+            variable: 'a',
+            value: {
+                type: 'CALL',
+                name: 'foobar',
+                arguments: [
+                    {type: 'NUMBER', value: 1},
+                    {type: 'NUMBER', value: 2},
+                    {type: 'NUMBER', value: 3},
+                ]
+            }
+        })
+    });
+
+    it('a = add(add(3, c), 1)', () => {
+        expect(parse(tokenize('a = add(add(3, c), 1)'))).toEqual({
+            type: 'ASSIGN',
+            variable: 'a',
+            value: {
+                type: 'CALL',
+                name: 'add',
+                arguments: [
+                    {
+                        type: 'CALL',
+                        name: 'add',
+                        arguments: [
+                            {type: 'NUMBER', value: 3},
+                            {type: 'VARIABLE', name: 'c'},
+                        ]
+                    }
+                    ,
+                    {type: 'NUMBER', value: 1}
+                ]
+            }
+        })
+    });
+});
+
+describe('compiler', () => {
+    it('a = foo(foo(1, 2), foo(3, 4))', () => {
+        expect(compile(parse(tokenize('a = foo(foo(1, 2), foo(3, 4))')))).toEqual(
+            'let a = foo(foo(1, 2), foo(3, 4))'
+        )
+    });
+    it('a = add(add(1, 2), add(3, 4))', () => {
+        expect(compile(parse(tokenize('a = add(add(1, 2), add(3, 4))')))).toEqual(
+            'let a = 1 + 2 + 3 + 4'
+        )
+    });
+
 });
--- example/minilang.ts
+++ example/minilang.ts
@@ -11,17 +11,17 @@ export function tokenize(source: string) {
     while(offset < source.length) {
 
         const token = null
-            ?? tryMatch('', /^[a-z]+/)
+            ?? tryMatch('NAME', /^[a-z]+/)
             ?? tryMatch('WHITESPACE', /^\s/)
-            ?? tryMatch('', /^=/)
-            ?? tryMatch('', /^\(/)
-            ?? tryMatch('', /^,/)
-            ?? tryMatch('', /^\d+/)
-            ?? tryMatch('', /^\)/)
+            ?? tryMatch('ASSIGN', /^=/)
+            ?? tryMatch('OPEN', /^\(/)
+            ?? tryMatch('COMMA', /^,/)
+            ?? tryMatch('NUMBER', /^\d+/)
+            ?? tryMatch('CLOSE', /^\)/)
 
         if (token) {
             offset += token.value.length;
-            if (token.type !== 'WHITESPACE') tokens.push(token.value);
+            if (token.type !== 'WHITESPACE') tokens.push(token);
             continue;
         }
 
@@ -29,4 +29,99 @@ export function tokenize(source: string) {
     }
 
     return tokens;
+}
+
+
+export function parse(tokens) {
+    let offset = 0;
+
+    function token(type) {
+        const token = tokens[offset];
+        if (token.type !== type) throw new Error(`unexpected token: ${JSON.stringify(token)} after: ${offset}`);
+        offset++;
+        return token.value;
+    }
+
+    function ASSIGNMENT() {
+        const variable = token('NAME')
+        token('ASSIGN')
+        const value = VALUE();
+
+        return {
+            type: 'ASSIGN',
+            variable: variable,
+            value: value
+        }
+    }
+
+    function VARIABLE() {
+        return {
+            type: 'VARIABLE',
+            name: token('NAME'),
+        }
+    }
+
+    function tryRule(rule) {
+        const _offset = offset;
+        try {
+            return rule();
+        } catch (e ) {
+            offset = _offset;
+            return null;
+        }
+    }
+
+    function VALUE() {
+        return tryRule(NUMBER) ?? tryRule(CALL) ?? VARIABLE()
+    }
+
+    function CALL() {
+        const name = token('NAME');
+        token('OPEN')
+
+        // arguments
+        const args = []
+        while(tokens[offset].type !== 'CLOSE') {
+            const argValue = VALUE();
+            args.push(argValue);
+
+            if (tokens[offset].type !== 'CLOSE') token('COMMA');
+        }
+
+        token('CLOSE');
+
+        return {
+            type: 'CALL',
+            arguments: args,
+            name: name,
+        }
+    }
+
+    function NUMBER() {
+        const value = token('NUMBER')
+        return {
+            type: 'NUMBER',
+            value: parseInt(value)
+        }
+    }
+
+    return ASSIGNMENT();
+}
+
+
+export function compile(node) {
+    switch (node.type) {
+        case 'ASSIGN':
+            return `let ${(node.variable)} = ${compile(node.value)}`;
+        case 'CALL':
+            if (node.name === 'add') {
+                return node.arguments.map(compile).join(' + ')
+            }
+            const args = node.arguments.map(compile).join(', ')
+            return`${node.name}(${args})`;
+        case 'NUMBER':
+            return `${node.value}`
+        default:
+            throw new Error(`Unknown node type: ${node.type}`)
+    }
 }
